93df22c017245fd6e714d6aa567ee2de6784ff3a
Chibuike Favour <iamkorra03@gmail.com>
2023-03-19 12:17:35 -0700

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+* main - Entry
+* Return: Always 0
+*/
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
