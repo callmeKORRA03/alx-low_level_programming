812fe96de23379565fc5155e9b48c44693df20d4
Chibuike Favour <iamkorra03@gmail.com>
2023-07-28 02:43:52 +0100

    main

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,26 +1,22 @@
 #include "variadic_functions.h"
-
+#include <stdarg.h>
 /**
- * sum_them_all - adds all the numbers
- * @n: the number of parameters passed
- * Return: Always 0
+ * sum_them_all - prototype that returns the sum of all its paramters.
+ * @n: The number of paramters passed to the function.
+ * @...: A variable number of paramters to calculate the sum of.
+ * Return: the sum of all parameters.
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum;
-	unsigned int i;
-	va_list ap;
+	va_list num;
+	unsigned int i, sum = 0;
 
-	va_start(ap, n);
-
-	if (n == 0)
-		return (0);
+	va_start(num, n);
 
 	for (i = 0; i < n; i++)
-		sum += va_arg(ap, int);
+		sum += va_arg(num, int);
 
-	va_end(ap);
+	va_end(num);
 
 	return (sum);
 }
--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -1,28 +1,26 @@
 #include "variadic_functions.h"
-
+#include <stdio.h>
+#include <stdarg.h>
 /**
- * print_numbers - prints numbers followed by a new line
- * @n: number of parameter
- * @separator: separator between two numbers
- * Return: Always 0
+ * print_numbers - a prototype that prints numbers
+ * @separator: string to be printed between numbers.
+ * @n: number of integers passed to the function.
+ * @...: variable of numbers to be printed.
  */
-
 void print_numbers(const char *separator, const unsigned int n, ...)
 {
+	va_list num;
 	unsigned int i;
-	va_list ap;
 
-	va_start(ap, n);
-
-	if (separator == NULL)
-		separator = "";
+	va_start(num, n);
 
 	for (i = 0; i < n; i++)
 	{
-		printf("%d", va_arg(ap, int));
-		if (i < n - 1)
+		printf("%d", va_arg(num, int));
+
+		if (i != (n - 1) && separator != NULL)
 			printf("%s", separator);
 	}
 	printf("\n");
-	va_end(ap);
+	va_end(num);
 }
